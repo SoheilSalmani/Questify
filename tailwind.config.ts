0f4b3a8d31915ce4f85c80274b3458064d952e0f
Soheil Salmani <salmani.soheil@gmail.com>
2024-04-29 22:31:43 +0200

    ui: add new fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,12 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        led: "The Led Display St",
+        alegreya: "Alegreya",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
